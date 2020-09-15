9ec846d02430bec3dbd8d34644fd20dea676ef15
amir gatenyu <gatenyu.developments@gmail.com>
2020-09-15 20:04:42 +0300

    footer mobile V done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,5 +2,21 @@ import React from "react";
 import "./footer.scss";
 
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="inner-footer">
+        <ul>
+          <span className="">spacex &copy; 2020</span>
+          <a href="#">twitter</a>
+          <a href="#">youtube</a>
+          <a href="#">instagram</a>
+          <a href="#">flickr</a>
+          <a href="#">linkedin</a>
+          <a href="#" className="privacy">
+            PRIVACY POLICY
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
 }
