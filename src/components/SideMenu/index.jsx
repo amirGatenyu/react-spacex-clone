df8d7fffb9824b0fb63062bd805f970a4ca46e22
amir gatenyu <gatenyu.developments@gmail.com>
2020-09-15 09:21:05 +0300

    side manu changes

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -8,7 +8,60 @@ export default function SideMenu(props) {
         props.open ? "side-menu-container open" : "side-menu-container"
       }
     >
-      Sidemenuddddddddddddddddddddddddd
+      <div className="inner-side-menu">
+        <a href="#">falcon 9</a>
+        <hr />
+        <a href="#">Falcon Heavy</a>
+        <hr />
+        <a href="#">dragon</a>
+        <hr />
+        <a href="#">starship</a>
+        <hr />
+        <a href="#">human spacelight</a>
+        <hr />
+        <a href="#">rideshare</a>
+        <hr />
+        <a href="#">mission</a>
+        <hr />
+        <a href="#">launches</a>
+        <hr />
+        <a href="#">careers</a>
+        <hr />
+        <a href="#">updates</a>
+        <hr />
+        <a href="#">shop</a>
+        <hr />
+      </div>
+      {/* <div
+        className={
+          props.close ? "side-menu-container close" : "side-menu-container"
+        }
+      >
+        <div className="inner-side-menu">
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+          <a href="#">falcon 9</a>
+          <hr />
+        </div>
+      </div> */}
     </div>
   );
 }
